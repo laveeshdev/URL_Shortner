8b3a61dcbf19db1a538930fb316fbc4eab3a5112
laveeshsanadhya <laveeshsanadhya@gmail.com>
2025-04-20 15:02:28 +0530

    defined the schema for our url in mongodb

---
+++ models/url.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const urlSchema = new mongoose.Schema({
+    shortId : {
+        type : "string" , 
+        required : true , 
+        unique : true , 
+    } , 
+    redirectUrl : {
+        type : "string" , 
+        required : true ,
+    } , 
+    visitHistory : [{
+        timestamp : {
+            type : Number , 
+        }
+    }]
+} , {
+    timestamps: true 
+}
+)
+
+
+const URL = mongoose.model('url' , urlSchema)
+
+
+mudule.exports = {
+    URL
+}
